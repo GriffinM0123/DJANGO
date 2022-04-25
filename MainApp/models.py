2696eabbbf27daecff1071035248084c8b6a8db2
Griffin McGuckin <Griffin_McGuckin1@baylor.edu>
2022-04-25 15:46:04 -0500

    Day 2

--- MainApp/models.py
+++ MainApp/models.py
@@ -15,6 +15,9 @@ class Entry(models.Model):
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        verbose_name_plural = 'Entries'
+
 
     def __str__(self):
         return f"{self.text[:50]}..."
